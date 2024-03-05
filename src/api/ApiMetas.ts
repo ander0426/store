9b0c5497589e55cc1e7cd097e357f978aa8f3fbf
ander0426 <ander0426@gmail.com>
2024-03-05 04:42:26 -0500

    dashboard consult users

--- src/api/ApiMetas.ts
+++ src/api/ApiMetas.ts
@@ -44,3 +44,45 @@ export const ApiMetasGuardar = async (body:any) => {
     return result
 }
 
+export const ApiMetasConsulUser = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    };
+    
+    const response = await fetch("https://yxth7wmpgeqra7pkmtsvnubrly0qdwvg.lambda-url.us-east-1.on.aws/api/v1/users/get_all", requestOptions);
+    const isJson = response.headers.get('content-type')?.includes('application/json');
+    const data = isJson && await response.json();
+    
+    const result = {
+      status: response.status,
+      data: data
+    }
+    
+    if (!response.ok) {
+        process.env.NODE_ENV === "development" && console.error('There was an error!', result);
+    }
+    return result
+}
+
+export const ApiMetasConsulUserMes = async (body: any) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+    };
+    
+    const response = await fetch("https://yxth7wmpgeqra7pkmtsvnubrly0qdwvg.lambda-url.us-east-1.on.aws/api/v1/users/total_sales", requestOptions);
+    const isJson = response.headers.get('content-type')?.includes('application/json');
+    const data = isJson && await response.json();
+    
+    const result = {
+      status: response.status,
+      data: data
+    }
+    
+    if (!response.ok) {
+        process.env.NODE_ENV === "development" && console.error('There was an error!', result);
+    }
+    return result
+}
--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,81 +1,134 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from '../navbar/Navbar'
+import { ApiMetasConsulUser, ApiMetasConsulUserMes } from '../../api/ApiMetas';
 
 export const Dashboard = () => {
 
-    const [cardShow, setCardShow] = useState(false);
-
-    const handleChangeUser = (id) => {
-        setCardShow(true)
-        console.log(id);
+    const [cardShow, setCardShow] = useState(false)
+    const [users, setUsers] = useState([])
+    const [cardsData, setCardsData] = useState({
+        "month": {
+            "first_day": "",
+            "last_day": "",
+            "ammount": 0,
+            "opportunities": []
+        },
+        "year": {
+            "first_day": "",
+            "last_day": "",
+            "opportunities": [
+                {
+                    "process_quantity": "",
+                    "assigned_user_id": "",
+                    "tipo_de_servicio_c": "",
+                    "amount_usdollar": 0
+                }
+            ]
         }
+    })
+
+    let USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    const consultUsers = async () => {
+        const user = await ApiMetasConsulUser().then(result => {
+            return result.data
+        })
+        setUsers(user)
+    }
+
+    const typeMeta = (type:any) => {
+        console.log(type)
+        switch (type) {
+            case "Big Deal":
+                return "BDL"
+                break;
+            case "HHT":
+                return "HHT"
+                break;
+            case "Residencial":
+                return "RST"
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    useEffect(() => {
+
+        consultUsers()
+
+    }, [])
+
+    const handleChangeUser = async (id) => {
+        let data = {
+            iduser: id
+        }
+        const metas = await ApiMetasConsulUserMes(data).then(result => {
+            return result.data
+        })
+        setCardsData(metas)
+        setCardShow(true)
+    }
     return (
         <>
             <Navbar />
             <div className="container-fluid">
                 <div className="row pt-4">
                     <div className="col-12">
-                    <div className="mb-3 ">
-                        <label className="form-label">Usuario:</label>
-                        <select className="form-select form-select-lg" aria-label="Default select example" onChange={(event) => handleChangeUser(event.target.value)}>
-                            <option selected disabled>Selecciona</option>
-                            <option value="1">Pedro</option>
-                            <option value="2">Maria</option>
-                            <option value="3">Jose</option>
-                        </select>
+                        <div className="mb-3 ">
+                            <label className="form-label">Usuario:</label>
+                            <select className="form-select form-select-lg" defaultValue={"00"} aria-label="Default select example" onChange={(event) => handleChangeUser(event.target.value)}>
+                                <option value="00" disabled>Selecciona</option>
+                                {
+                                    users.map((user, i) =>
+                                        <option key={i} value={user.id}>{user.first_name + " " + user.last_name}</option>
+                                    )
+                                }
+                            </select>
                         </div>
                     </div>
                     {cardShow && (
-                    <div className='row'>
-                    <div className="col-3">
-                        <div className="card shadow py-3">
-                            <div className="card-body">
-                                <h3 className=" text-secondary">Metas Totales Mes Actual</h3>
-                                <p className="h4">Procesos:</p>
-                                <p className="h4">Monto: </p>
-                                <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-3 me-2 h3 text-center py-2 text-light">
-                                    RES
-                                </span>
-                            </div>
+                        <div className='row'>
+                            {
+                                cardsData.month.opportunities.map((item, i) =>
+                                    <div className="col-3" key={i}>
+                                        <div className="card shadow py-3">
+                                            <div className="card-body">
+                                                <h3 className=" text-secondary">Metas Totales Mes Actual</h3>
+                                                <p className="h4">Procesos: {item.process_quantity}</p>
+                                                <p className="h4">Monto: {USDollar.format(item.amount_usdollar)}</p>
+
+                                                <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-3 me-2 h3 text-center py-2 text-light">
+                                                    {typeMeta(item.tipo_de_servicio_c)}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            {
+                                cardsData.year.opportunities.map((item, i) =>
+                                    <div className="col-3" key={i}>
+                                        <div className="card shadow py-3">
+                                            <div className="card-body">
+                                                <h3 className=" text-secondary">Metas Totales YTD</h3>
+                                                <p className="h4">Procesos: {item.process_quantity}</p>
+                                                <p className="h4">Monto: {USDollar.format(item.amount_usdollar)}</p>
+
+                                                <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-3 me-2 h3 text-center py-2 text-light">
+                                                    {typeMeta(item.tipo_de_servicio_c)}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+
                         </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card shadow py-3">
-                                <div className="card-body">
-                                    <h3 className=" text-secondary">Metas Totales Mes Actual</h3>
-                                    <p className="h4">Procesos:</p>
-                                    <p className="h4">Monto: </p>
-                                    <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-3 me-2 h3 text-center py-2 text-light">
-                                        RES
-                                    </span>
-                                </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card shadow py-3">
-                            <div className="card-body">
-                                <h3 className=" text-secondary">Metas Totales Mes Actual</h3>
-                                <p className="h4">Procesos:</p>
-                                <p className="h4">Monto: </p>
-                                <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-3 me-2 h3 text-center py-2 text-light">
-                                    RES
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="card shadow py-3">
-                            <div className="card-body">
-                                <h3 className=" text-secondary">Metas Totales Mes Actual</h3>
-                                <p className="h4">Procesos:</p>
-                                <p className="h4">Monto: </p>
-                                <span className="rounded-circle bg-secondary position-absolute span-rounded top-0 end-0 mt-2 me-2 h3 text-center py-2 text-light">
-                                    RES
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
                     )}
                 </div>
             </div>
